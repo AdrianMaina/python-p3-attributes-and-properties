aa0af3d558191939f3157012a68a3139c83ecc40
Haron M Maina <haronmmaina@Harons-MacBook-Air.local>
2025-05-12 15:11:33 +0300

    Makes changes

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 APPROVED_BREEDS = [
+    "Unknown",
     "Mastiff",
     "Chihuahua",
     "Corgi",
@@ -12,4 +13,19 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Unknown", breed="Unknown"):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+            self.name = "Invalid name"
+        else:
+            self.name = name
+
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            self.breed = "Invalid breed"
+        else:
+            self.breed = breed
+
+Fido = Dog("", "Beagle")
+print(Fido.name)
+print(Fido.breed)
--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 APPROVED_JOBS = [
+    "Unknown",
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -16,4 +17,14 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name="Unknown",job="Unknown"):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+            self.name="Invalid name"
+        else:
+            self.name=name.title()
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            self.job="Invalid job"
+        else:
+            self.job=job
